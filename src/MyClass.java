3ff182c25926355904e14d26b456ad38eead1b0f
aminul159 <aminulislam159@gmail.com>
2017-03-18 13:19:17 +0600

    Hello First Commit

---
+++ src/MyClass.java
@@ -0,0 +1,14 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10, b=30;
+		int add;
+		add=a-b;
+		System.out.println(add);
+		
+	}
+
+}
